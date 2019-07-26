bc4849347f7fc496b74a743b7be721701d07e4a5
pinehat <pinehat@gmail.com>
2019-07-26 13:27:48 -0400

    work in progress

--- hp_employees_schema.sql
+++ hp_employees_schema.sql
@@ -1,86 +1,11 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/XYIE09
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+--List the department of each employee with the following 
+--information: employee number, last name, first name, and 
+--department name.
 
--- Modify this code to update the DB schema diagram.
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
-
-CREATE TABLE "department" (
-    "dept_no" VARCHAR(4)   NOT NULL,
-    "dept_name" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_department" PRIMARY KEY (
-        "dept_no"
-     )
-);
-
-CREATE TABLE "dept_emp" (
-    "emp_no" INTEGER   NOT NULL,
-    "dept_no" VARCHAR(4)   NOT NULL,
-    "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "emp_no","from_date"
-     )
-);
-
-CREATE TABLE "employees" (
-    "emp_no" INTEGER   NOT NULL,
-    "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "gender" VARCHAR(1)   NOT NULL,
-    "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
-CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR(4)   NOT NULL,
-    "emp_no" INTEGER   NOT NULL,
-    "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dept_no","from_date"
-     )
-);
-
-CREATE TABLE "salaries" (
-    "emp_no" INTEGER   NOT NULL,
-    "salary" INTEGER   NOT NULL,
-    "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL,
-    CONSTRAINT "pk_salaries" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
-CREATE TABLE "titles" (
-    "emp_no" INTEGER   NOT NULL,
-    "title" VARCHAR   NOT NULL,
-    "from_date" DATE   NOT NULL,
-    "to_date" DATE   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "emp_no","from_date"
-     )
-);
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "department" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "department" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
+SELECT a.dept_no as "Dept. No.", a.dept_name as "Dept. Name", 
+c.emp_no as "Mgr's Emp. No.", c.first_name as "Mgr First Name", 
+c.last_name as "Mgr Last Name", b.from_date as "Employment Start Date", 
+b.to_date as "Employment End Date"
+FROM department a
+JOIN dept_manager b ON a.dept_no = b.dept_no
+JOIN employees c ON b.emp_no = c.emp_no;
